4d2403e60322af152dd2d4d3c1b21c805305221c
RiosGrand <JRios@kippnyccp.org>
2015-03-19 13:26:17 -0400

    Update javascript.js

--- Memory/javascript.js
+++ Memory/javascript.js
@@ -1,11 +1,32 @@
-
 $(document).ready(function(){
   var card_array = ["Tokyo Ghoul", "Tokyo Ghoul", "DBZ", "DBZ", "Bobobo", "Bobobo", "Broly", "Broly", "Levi", "Levi", "Grandma", "Grandma", "Blackarot", "Blackarot", "Shamu", "Shamu", "It G Ma", "It G Ma", "Ye", "Ye"];
   var click_count = 0;
   var click_1 = 'nothing';
   var click_2 = 'nothing';
-  for(i in card_array){
-    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  
+  function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+	var fluffled_array = shuffle(card_array);
+  
+  for(i in fluffled_array){
+    $('#card_holder').append('<div class="card"><p>'+fluffled_array[i]+'</p></div>');
   };
   $('.card').click(function(){
     if(click_count == 0){
